c3ee3892deaf9a7a5d1d8c1f0d124e15c7f9d868
D9RS <d9rs.dev@gmail.com>
2020-08-07 22:49:21 +0500

    fix error in filename

--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,10 @@ const AuthController = require('../controllers/AuthController');
 const router = express.Router();
 
 const isAuthorized = require('../middlewares/isAuthorized');
-const isNotAuthized = require('../middlewares/isNotAuthized');
+const isNotAuthorized = require('../middlewares/isNotAuthorized');
 
-router.post('/signup', isNotAuthized, AuthController.signUp);
-router.post('/signin', isNotAuthized, AuthController.signIn);
+router.post('/signup', isNotAuthorized, AuthController.signUp);
+router.post('/signin', isNotAuthorized, AuthController.signIn);
 router.post('/signout', isAuthorized, AuthController.signOut);
 
 module.exports = router;
